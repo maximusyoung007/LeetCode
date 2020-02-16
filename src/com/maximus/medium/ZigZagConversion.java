a70df9d1587f20f35705593e44120cda587e82cd
maximus <1091261998@qq.com>
2020-02-16 18:28:38 +0800

    0006

---
+++ src/com/maximus/medium/ZigZagConversion.java
@@ -0,0 +1,10 @@
+package com.maximus.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigZagConversion {
+    public String convert(String s,int numRows) {
+       return s;
+    }
+}
