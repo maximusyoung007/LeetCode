baf0fa0e46bcd67eed19957b2092376435742b83
maximus <1091261998@qq.com>
2020-11-06 23:02:05 +0800

    0003

--- c++/0003.cpp
+++ c++/0003.cpp
@@ -1,23 +1,21 @@
 #include<string>
+#include<set>
 #include "0003.h"
 using namespace std;
 
 int Solution0003::lengthOfLongestsubString(string s) {
-    //abcabcbb
-    int len = 0;
-    int j = 1;
-    for(int i = 0;i < s.length();i++) {
-        int temp_len = j - i;
-        while(j < s.length() && s[j] != s[i]) {
-            j++;
-            temp_len++;
-            if(s[j] == s[j + 1] && j+1 <= s.length() - 2) {
-                i = j+1;
-                j = i+1;
-            }
+    set<int> set;
+    int len = 0,right = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (i != 0) {
+            set.erase(s[i - 1]);
         }
-        if(temp_len > len) {
-            len = temp_len;
+        while (right < s.size() && !set.count(s[right])) {
+            set.insert(s[right]);
+            right++;
+        }
+        if (right - i > len) {
+            len = right - i;
         }
     }
     return len;
