5033e9bfe9b3234602978700afdf288de84fc96e
maximus <1091261998@qq.com>
2020-11-11 21:13:37 +0800

    commit 0371 0415

---
+++ c++/0415.h
@@ -0,0 +1,9 @@
+#ifndef C___0415_H
+#define C___0415_H
+#include<string>
+using namespace std;
+class Solution0415{
+public:
+    string addStrings(string num1,string num2);
+};
+#endif //C___0415_H
