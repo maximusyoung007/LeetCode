36a21eb75a2d1d341e5574a73430edb86538e7c0
maximus <1091261998@qq.com>
2020-11-16 19:51:54 +0800

    0016 unfinished

---
+++ c++/0016.cpp
@@ -0,0 +1,14 @@
+#include"0016.h"
+#include "limits.h"
+using namespace std;
+
+int Solution0016 ::threeSumClosest(vector<int> &nums, int target) {
+    if(nums.size() < 3)
+        return {};
+    sort(nums.begin(),nums.end());
+    int result,minimum = INT_MAX;
+    for(int i = 0;i < nums.size();i++) {
+
+    }
+    return result;
+}
---
+++ c++/0016.h
@@ -0,0 +1,9 @@
+#include<vector>
+using namespace std;
+#ifndef C___0016_H
+#define C___0016_H
+class Solution0016 {
+public:
+    int threeSumClosest(vector<int>& nums, int target);
+};
+#endif //C___0016_H
