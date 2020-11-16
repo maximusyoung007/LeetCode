76429af7060c582d7ac6bb6a8ad70c4bbe8e92cb
maximus <1091261998@qq.com>
2020-11-16 23:38:56 +0800

    0016

--- c++/0016.cpp
+++ c++/0016.cpp
@@ -1,14 +1,36 @@
 #include"0016.h"
 #include "limits.h"
+#include<math.h>
+#include<algorithm>
 using namespace std;
 
 int Solution0016 ::threeSumClosest(vector<int> &nums, int target) {
     if(nums.size() < 3)
-        return {};
+        return 0;
     sort(nums.begin(),nums.end());
-    int result,minimum = INT_MAX;
+    int minimum = INT_MAX,result = 0;
     for(int i = 0;i < nums.size();i++) {
-
+        int left = i + 1,right = nums.size() - 1;
+        while(left < right) {
+            int sum = nums[left] + nums[right] + nums[i];
+            if(sum == target) {
+                return target;
+            }
+            else if(sum > target) {
+                right--;
+                if(abs(sum - target) < abs(minimum)) {
+                    minimum = sum - target;
+                    result = sum;
+                }
+            }
+            else if(sum < target) {
+                left++;
+                if(abs(sum - target) < abs(minimum)) {
+                    minimum = sum - target;
+                    result = sum;
+                }
+            }
+        }
     }
     return result;
 }
