e817b20fef7137eca93e960e1d9a45819d3ed61b
maximus <1091261998@qq.com>
2020-12-05 12:29:21 +0800

    .

--- c++/0036.cpp
+++ c++/0036.cpp
@@ -5,7 +5,10 @@ bool Solution0036::isValidSudoku(vector<vector<char>> &board) {
     int area[9][9] = {0};
     for(int i = 0;i < 9;i++) {
         for(int j = 0;j < 9;j++) {
-            int num = board[i][j] - '0';
+            if(board[i][j] == '.') {
+                continue;
+            }
+            int num = (board[i][j] - '0') - 1;
             int index = (i / 3) * 3 + j / 3;
             if(row[i][num] == 1 || line[num][i] == 1 || area[index][num] == 1) {
                 return false;
