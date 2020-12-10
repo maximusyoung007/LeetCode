dc0357add5979f6c833628d756df8022c0689a15
maximus <1091261998@qq.com>
2020-12-10 22:37:55 +0800

    0043 haven't tested

--- c++/0043.cpp
+++ c++/0043.cpp
@@ -1,7 +1,16 @@
 #include "0043.h"
 #include <algorithm>
 string Solution::multiply(string num1, string num2) {
-
+    reverse(num2.begin(),num2.end());
+    string result = "";
+    for(int i = 0;i < num2.size();i++) {
+        string result1 = multiplyToOne(num1,num2[i]);
+        for(int j = 0;j < i;j++) {
+            result1 += "0";
+        }
+        result = add(result,result1);
+    }
+    return result;
 }
 
 string Solution::multiplyToOne(string num1, char num2) {
@@ -18,12 +27,30 @@ string Solution::multiplyToOne(string num1, char num2) {
         } else {
             carry = 0;
         }
-        result += temp;
+        result += to_string(temp);
     }
     reverse(result.begin(),result.end());
     return result;
 }
 
 string Solution::add(string num1, string num2) {
-
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+    int k = 0,carry = 0;
+    string result;
+    while(num1.size() > k || num2.size() > k) {
+        int a = num1.size() > k ? num1[k] - '0' : 0;
+        int b = num2.size() > k ? num2[k] - '0' : 0;
+        int temp = a + b + carry;
+        if(temp > 10) {
+            temp = temp % 10;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+        result += to_string(temp);
+        k++;
+    }
+    reverse(result.begin(),result.end());
+    return result;
 }
