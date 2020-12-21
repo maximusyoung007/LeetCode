cfe0c5be792840e2152053b27d1b65b2d0598d34
maximus <1091261998@qq.com>
2020-12-21 17:30:43 +0800

    94

---
+++ c++/0094.cpp
@@ -0,0 +1,18 @@
+#include "tree.h"
+#include <vector>
+using namespace std;
+
+class Solution0094 {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        ldr(root,result);
+        return result;
+    }
+
+    void ldr(TreeNode *root,vector<int> &result) {
+        if(root != nullptr) result.push_back(root->val);
+        if(root->left != nullptr) ldr(root->left,result);
+        if(root->right != nullptr) ldr(root->right,result);
+    }
+};
