42c315371b4de81c5b3c69ac4ed7333d4c207425
maximus <1091261998@qq.com>
2020-12-23 21:56:13 +0800

    91

--- c++/0091.cpp
+++ c++/0091.cpp
@@ -4,31 +4,22 @@ using namespace std;
 class Solution0091 {
 public:
     int numDecodings(string s) {
-        if(s == "0") return 0;
-        if(s[0] == '0') {
-            int index = 0;
-            for(int i = 0;i < s.size();i++) {
-                if(s[i] == '0') {
-                    index++;
+        if(s[0] == '0') return 0;
+        int a0 = 1,a1 = 1;
+        for(int i = 1;i < s.size();i++) {
+            int temp = a1;
+            if(s[i] == '0') {
+                if(s[i-1] == '1' || s[i-1] == '2') {
+                    a1 = a0;
                 } else {
-                    break;
+                    return 0;
+                }
+            } else {
+                if(s[i-1] == '1' || (s[i-1] == '2' && s[i] >= '1' && s[i] <= '6')) {
+                    a1 = a0 + a1;
                 }
             }
-            s = s.substr(index,s.size() - index);
-        }
-        if(s.size() == 1) return 1;
-        if(s == "") return 0;
-        int a0 = 1,a1;
-        for(int i = 1;i < s.size();i++) {
-            int s1 = s[i-1] - '0',s2 = s[i] - '0';
-            if(s2 == 0) {
-                a1 = a0;
-            } else if(s1 * 10 + s2 <= 26) {
-                a1 = a0 + 1;
-            } else {
-                a1 = a0;
-            }
-            a0 = a1;
+            a0 = temp;
         }
         return a1;
     }
