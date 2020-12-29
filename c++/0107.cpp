b9afbcdc8ee0de8706cdde6b9c3bb841cbdff3d1
maximus <1091261998@qq.com>
2020-12-29 11:47:57 +0800

    102 103 107

---
+++ c++/0107.cpp
@@ -0,0 +1,41 @@
+#include "tree.h"
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution0107 {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> result;
+        if(root == nullptr) {
+            return result;
+        }
+        else {
+            q.push(root);
+        }
+        while(!q.empty()) {
+            int tempSize = q.size();
+            vector<int> partResult;
+            while(tempSize > 0) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp != nullptr) {
+                    partResult.push_back(temp->val);
+                }
+                if (temp != nullptr && temp->left != nullptr) {
+                    q.push(temp->left);
+                }
+                if (temp != nullptr && temp->right != nullptr) {
+                    q.push(temp->right);
+                }
+                tempSize--;
+                if(tempSize == 0) {
+                    result.push_back(partResult);
+                }
+            }
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
