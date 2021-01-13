e3322e9da507dc98fb506ce1196038f7cb7c9de2
maximus <1091261998@qq.com>
2021-01-13 23:24:05 +0800

    0010

---
+++ c++/0010.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution0010 {
+public:
+    bool isMatch(string s, string p) {
+        int len1 = s.size();
+        int len2 = p.size();
+        bool dp[40][40];
+        dp[0][0] = true;
+        for(int j = 0;j <= len2;j++) {
+            if(j > 2) {
+                dp[0][j] = dp[0][j - 1] == '*' && dp[0][j - 2];
+            }
+        }
+        for(int i = 0;i <= len1;i++) {
+            dp[i][0] = false;
+        }
+
+        for(int i = 1;i <= len1;i++) {
+            for(int j = 1;j <= len2;j++) {
+                if (p[j - 1] != '*') {
+                    if(p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    if(p[j - 2] != s[i - 1] && p[j - 2] != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 2] || dp[i][j - 1] || dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+};
