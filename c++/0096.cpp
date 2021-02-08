709a42f92ef959e74d849f61ad4a6a6949ee688b
maximus <1091261998@qq.com>
2021-02-08 11:29:17 +0800

    finished 96 modified 93

---
+++ c++/0096.cpp
@@ -0,0 +1,19 @@
+#include "tree.h"
+#include<vector>
+using namespace std;
+
+class Solution0096 {
+public:
+    int numTrees(int n) {
+        vector<int> g(n + 1, 0);
+        g[0] = 1;
+        g[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                g[i] += g[j - 1] * g[i - j];
+            }
+        }
+        return g[n];
+    }
+};
