75fc6cf6c28d944c9b774d6e154e7d3de3e03a26
maximus <1091261998@qq.com>
2021-03-20 23:09:46 +0800

    137

---
+++ c++/0137.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+class Solution0137 {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+};
