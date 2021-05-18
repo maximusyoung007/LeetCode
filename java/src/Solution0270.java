3b0dcdeba013dca5d9ef977160d8e0e54371d553
maximus <1091261998@qq.com>
2021-05-18 23:42:42 +0800

    270

---
+++ java/src/Solution0270.java
@@ -0,0 +1,12 @@
+import dataStructure.TreeNode;
+
+public class Solution0270 {
+    public int closestValue(TreeNode root, double target) {
+        int closest = root.val;
+        while (root != null) {
+            closest = Math.abs(root.val - target) < Math.abs(closest - target) ? root.val : closest;
+            root = root.val > target ? root.left : root.right;
+        }
+        return closest;
+    }
+}
