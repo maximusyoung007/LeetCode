ddae932fc2b4b21af374e77a33cb50046ee7bdb2
maximus <1091261998@qq.com>
2021-05-20 22:34:06 +0800

    261 290

--- java/src/Solution0261.java
+++ java/src/Solution0261.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 261. 以图判树
  * 给定从 0 到 n-1 标号的 n 个结点，和一个无向边列表（每条边以结点对来表示），请编写一个函数用来判断这些边是否能够形成一个合法有效的树结构。
@@ -12,6 +14,40 @@
  * 输出: false
  */
 public class Solution0261 {
-    int[] fa = new int[100];
-
+    int f[];
+    void init(int n) {
+        for (int i = 0; i < n; i++) {
+            f[i] = i;
+        }
+    }
+    int find(int x) {
+        if (x == f[x]) {
+            return x;
+        }
+        return f[x] = find(f[x]);
+    }
+    void merge(int x, int y) {
+        int fx = find(x);
+        int fy = find(y);
+        f[fx] = fy;
+    }
+    int count() {
+        int count = 0;
+        for (int i = 0; i < f.length; i++) {
+            if (find(i) == i) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public boolean validTree(int n, int[][] edges) {
+        f = new int[n];
+        init(n);
+        for (int i = 0; i < edges.length; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            merge(a, b);
+        }
+        return edges.length + 1 == n && count() == 1;
+    }
 }
--- java/src/Solution0290.java
+++ java/src/Solution0290.java
@@ -1,2 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution0290 {
+    public boolean wordPattern(String pattern, String s) {
+        String[] ss = s.split(" ");
+        if (pattern.length() != ss.length) {
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char tc = pattern.charAt(i);
+            String ts = ss[i];
+            if (map.containsKey(tc)) {
+                if (!map.get(tc).equals(ts)) return false;
+            } else {
+                //tc 和ts对应，但是存在一个ts，不和tc对应，错误
+                if (map2.containsKey(ts) && map2.get(ts) != tc) {
+                    return false;
+                }
+                map.put(tc, ts);
+                map2.put(ts, tc);
+            }
+        }
+        return true;
+    }
 }
