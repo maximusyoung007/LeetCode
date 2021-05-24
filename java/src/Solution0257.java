4d9f532961b5ac91d7af73f94b25a66910899a28
maximus <1091261998@qq.com>
2021-05-24 21:48:22 +0800

    257

--- java/src/Solution0257.java
+++ java/src/Solution0257.java
@@ -1,28 +1,45 @@
 import dataStructure.TreeNode;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution0257 {
     StringBuilder s = new StringBuilder("");
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        dfs(result, s, root);
-        return result;
+        List<List<String>> result = new ArrayList<>();
+        List<String> partResult = new ArrayList<>();
+        List<String> r = new ArrayList<>();
+        dfs(result, partResult, root);
+        for (List<String> l : result) {
+            String ts = "";
+            for (int i = 0; i < l.size() - 1; i++) {
+                ts += l.get(i) + "->";
+            }
+            ts += l.get(l.size() - 1);
+            r.add(ts);
+        }
+        return r;
     }
 
-    void dfs(List<String> result, StringBuilder s, TreeNode root) {
-        s.append(root.val);
+    void dfs(List<List<String>> result, List<String> partResult, TreeNode root) {
+        int val = root.val;
+        Integer v = val;
+        partResult.add(v.toString());
         if (root.left != null) {
-            dfs(result, s, root.left);
+            dfs(result, partResult, root.left);
+            partResult.remove(partResult.size() - 1);
         }
-        s.deleteCharAt(s.length() - 1);
         if (root.right != null) {
-            dfs(result, s, root.right);
+            dfs(result, partResult, root.right);
+            partResult.remove(partResult.size() - 1);
         }
-        s.deleteCharAt(s.length() - 1);
         if (root.left == null && root.right == null) {
-            result.add(s.toString());
+            List<String> tempPartResult = new ArrayList<>();
+            for (String s : partResult) {
+                tempPartResult.add(s);
+            }
+            result.add(tempPartResult);
             return;
         }
     }
