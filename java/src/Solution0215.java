cc7d795c1a259ee0713350982a7522e7708af1d3
maximus <1091261998@qq.com>
2021-07-28 10:23:47 +0800

    215 api选手 216 深度优先遍历

---
+++ java/src/Solution0215.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+ *
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ * Example 1:
+ * Input: nums = [3,2,1,5,6,4], k = 2
+ * Output: 5
+ *
+ * Example 2:
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+ * Output: 4
+ *  
+ * Constraints:
+ * 1 <= k <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution0215 {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
