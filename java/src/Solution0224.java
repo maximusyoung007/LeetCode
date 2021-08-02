bf9f104d9229635aacd65506c0a0bf3a13c37332
maximus <1091261998@qq.com>
2021-08-02 14:31:59 +0800

    224 栈

---
+++ java/src/Solution0224.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+
+/**
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ *
+ * 示例 1：
+ * 输入：s = "1 + 1"
+ * 输出：2
+ *
+ * 示例 2：
+ * 输入：s = " 2-1 + 2 "
+ * 输出：3
+ *
+ * 示例 3：
+ * 输入：s = "(1+(4+5+2)-3)+(6+8)"
+ * 输出：23
+ *  
+ * 提示：
+ * 1 <= s.length <= 3 * 105
+ * s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+ * s 表示一个有效的表达式
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/basic-calculator
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution0224 {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        int result = 0;
+        int sign = 1;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == ' ') {
+                i++;
+                continue;
+            } else if (s.charAt(i) == '+') {
+                sign = stack.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -stack.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                stack.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                sign = stack.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                result += sign * num;
+            }
+        }
+        return result;
+    }
+}
