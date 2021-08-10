8d40ec815234c484b78db9295de817b4895e81f8
maximus <1091261998@qq.com>
2021-08-10 11:33:42 +0800

    234 双指针翻转链表的一半

---
+++ java/src/Solution0234.java
@@ -0,0 +1,53 @@
+import dataStructure.ListNode;
+
+/**
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ * 输入: 1->2
+ * 输出: false
+ *
+ * 示例 2:
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution0234 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode restHalf = slow.next;
+        ListNode reverse = reverseList(restHalf);
+        while (reverse != null) {
+            if (reverse.val != head.val) {
+                return false;
+            }
+            reverse = reverse.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = pre;
+            pre = current;
+            current = temp;
+        }
+        return pre;
+    }
+}
