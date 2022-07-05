cefdf616b8a33f0c153424d6a103f0b918ae868c
maximus <1091261998@qq.com>
2022-07-05 10:43:03 +0800

    729

---
+++ GoLang/solution/solution0729.go
@@ -0,0 +1,18 @@
+package solution
+
+type pair struct{ start, end int }
+type MyCalendar []pair
+
+func Constructor0729() MyCalendar {
+	return MyCalendar{}
+}
+
+func (c *MyCalendar) Book(start, end int) bool {
+	for _, p := range *c {
+		if p.start < end && start < p.end {
+			return false
+		}
+	}
+	*c = append(*c, pair{start, end})
+	return true
+}
