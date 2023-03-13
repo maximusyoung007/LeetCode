0046300bfcef4f5bd67ee1f419786ed0dac17485
maximus <1091261998@qq.com>
2023-03-13 23:18:29 +0800

    提交main函数和CMakeLists

---
+++ c++/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// #include "0954.cpp"
+// #include "0736.cpp"
+//#include "0187.cpp"
+using namespace std;
+#include"2383.c"
+int main() {
+//    Solution0187 solution0187;
+//    string s = "AAAAAAAAAAAAA";
+//    vector<string> vec = solution0187.findRepeatedDnaSequences(s);
+//    for (string s : vec) {
+//        cout << s << endl;
+    // Solution solution;
+    // string s = "(let x 2 (mult x (let x 3 y 4 (add x y))))";
+    // cout << solution.evaluate(s) << endl;
+    cout << "hello world" << endl;
+    int energy[] = {1,4,3,2};
+    int experience[] = {2,6,3,1};
+    int *e = energy;
+    int *ex = experience;
+    // for (int i = 0; i < 2; i++) {
+    //     cout << *(energy+i) << endl;
+    // }
+    cout << minNumberOfHours(5,3,e,4,ex,4) << endl;
+}
