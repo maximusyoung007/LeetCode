ccfddad62252c54066f5cc495ead2bb709dcf4b3
maximus <1091261998@qq.com>
2023-06-11 23:57:02 +0800

    2064

--- GoLang/solution/solution2064.go
+++ GoLang/solution/solution2064.go
@@ -1,27 +1,62 @@
 package solution
 
-import "sort"
+import (
+	"sort"
+)
 
-func minimizedMaximum(n int, quantities []int) int {
+func MinimizedMaximum(n int, quantities []int) int {
 	sort.Ints(quantities)
-	left, right := 0, quantities[len(quantities)-1]
+	left, right := 0, quantities[len(quantities)-1]+1
 	for left < right {
-
+		mid := (left + right + 1) / 2
+		if canDeliver(n, quantities, mid) {
+			right = mid - 1
+		} else {
+			left = mid
+		}
 	}
+	return right + 1
 }
 
-func canDeliver(n int, quntities []int, x int) {
-	i, k := 1, 0
+func canDeliver(n int, quntities []int, x int) bool {
+	//模拟每次分配x后，最后i和quntities的情况
+	i, k := 0, 0
 	le := len(quntities)
-	for i <= n && k < le {
-		if quntities[k] >= x {
+	q1 := make([]int, le)
+	copy(q1, quntities)
+	for i < n && k < le {
+		if q1[k] >= x {
 			i++
-			quntities[k] -= x
-		} else if quntities[k] < x && quntities[k] >= 0 {
+			q1[k] -= x
+			if q1[k] == 0 {
+				k++
+			}
+		} else if q1[k] < x && q1[k] >= 0 {
 			i++
-			quntities[k] = 0
 			k++
 		}
 	}
-	if i 
+	//x太大，不够分, 当作正确情况处理，x向小端移动
+	if i < n {
+		return true
+	}
+	//x 太小，还有剩下的
+	if k < le {
+		return false
+	}
+	//x太小，还有剩的
+	if k == le {
+		k -= 1
+		if q1[k] > x {
+			return false
+		}
+	}
+	return true
+
+	//模拟每次分x个，可以分多少个商店
+	//count := 0
+	//for _, q := range quntities {
+	//	count += (q-1)/x + 1
+	//}
+	//return count <= n
 }
