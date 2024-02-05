05cae4ad94d78fa1c542d2f57d760afa56906ec2
maximus <1091261998@qq.com>
2024-02-06 00:02:16 +0800

    2865 不太好的解决方案

--- golangR2/main.go
+++ golangR2/main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	fmt.Println("leetcode goLang round2")
 
-	maxHeight := []int{5, 3, 4, 1, 1}
+	//maxHeight := []int{5, 3, 4, 1, 1}
+	//maxHeight := []int{6, 5, 3, 9, 2, 7}
+	//maxHeight := []int{3, 2, 5, 5, 2, 3}
+	maxHeight := []int{5, 2, 4, 4}
 	println(solution.MaximumSumOfHeights(maxHeight))
 }
--- golangR2/solution/solution2865.go
+++ golangR2/solution/solution2865.go
@@ -1,9 +1,12 @@
 package solution
 
-func MaximumSumOfHeights(maxHeights []int) int {
+func MaximumSumOfHeights(maxHeights []int) int64 {
 	length := len(maxHeights)
 	stack := make([]int, 0)
 	leftRes, rightRes := make([]int, length), make([]int, length)
+
+	//左右两边都是递增的，先依次计算左边的，再计算右边的，再计算左右相加的，减去本身的
+	//stack记录下标好像效率更高，但是我不想优化了
 
 	for i := 0; i < length; i++ {
 		//如果是第一个元素，入栈
@@ -27,39 +30,42 @@ func MaximumSumOfHeights(maxHeights []int) int {
 				stack = append(stack, maxHeights[i])
 				leftRes[i] += maxHeights[i]
 			}
+			stack = append(stack, maxHeights[i])
 		}
 	}
 
 	stack = make([]int, 0)
 
 	for i := length - 1; i >= 0; i-- {
-		k := length - i - 1
+		//k := length - i - 1
 		if len(stack) == 0 {
 			stack = append(stack, maxHeights[i])
-			rightRes[k] = maxHeights[i]
+			rightRes[i] = maxHeights[i]
 		} else if maxHeights[i] >= stack[len(stack)-1] {
 			//如果大于栈顶元素，直接入栈
 			stack = append(stack, maxHeights[i])
-			rightRes[k] = rightRes[k-1] + maxHeights[i]
-		} else if len(stack) > 0 && maxHeights[i] < stack[len(stack)-1] {
+			rightRes[i] = rightRes[i+1] + maxHeights[i]
+		} else if maxHeights[i] < stack[len(stack)-1] {
 			count := 0
-			rightRes[k] = rightRes[k-1] + maxHeights[i]
-			for maxHeights[i] < stack[len(stack)-1] {
+			rightRes[i] = rightRes[i+1] + maxHeights[i]
+			for len(stack) > 0 && maxHeights[i] < stack[len(stack)-1] {
 				count++
-				rightRes[k] -= stack[len(stack)-1]
+				rightRes[i] -= stack[len(stack)-1]
 				stack = stack[0 : len(stack)-1]
 			}
 			for j := 0; j < count; j++ {
 				stack = append(stack, maxHeights[i])
-				rightRes[k] += maxHeights[i]
+				rightRes[i] += maxHeights[i]
 			}
+			stack = append(stack, maxHeights[i])
 		}
 	}
 
-	max := 0
+	var max int64
+	max = 0
 	for i := 0; i < length; i++ {
-		if leftRes[i]+rightRes[i] > max {
-			max = leftRes[i] + rightRes[i]
+		if int64(leftRes[i])+int64(rightRes[i])-int64(maxHeights[i]) > max {
+			max = int64(leftRes[i]) + int64(rightRes[i]) - int64(maxHeights[i])
 		}
 	}
 
