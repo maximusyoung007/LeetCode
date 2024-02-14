f27c8651ed524a2ae1a52929c780f693895f9b29
maximus <1091261998@qq.com>
2024-02-14 23:54:51 +0800

    452

--- JavaR2/src/Main.java
+++ JavaR2/src/Main.java
@@ -17,9 +17,18 @@ public class Main {
 //		Solution0455 solution0455 = new Solution0455();
 //		System.out.println(solution0455.findContentChildren(g, s));
 
-		Solution0605 solution0605 = new Solution0605();
+//		Solution0605 solution0605 = new Solution0605();
 //		int[] arr = new int[] {1, 0, 0, 0, 1};
-		int[] arr = new int[] {0};
-		System.out.println(solution0605.canPlaceFlowers(arr, 2));
+//		int[] arr = new int[] {0};
+//		System.out.println(solution0605.canPlaceFlowers(arr, 2));
+
+//		int[][] points = new int[][] {{10, 16}, {2,8}, {1,6}, {7,12}};
+//		int[][] points = new int[][] {{1, 2}, {3,4}, {5,6}, {7,8}};
+//		int[][] points = new int[][] {{1, 2}, {2,3}, {3,4}, {4,5}};
+//		int[][] points = new int[][] {{2, 3}, {2,3}};
+//		int[][] points = new int[][] {{1, 2}, {3,4}};
+		int[][] points = new int[][] {{-2147483646,-2147483645}, {2147483646,2147483647}};
+		Solution0452 solution0452 = new Solution0452();
+		System.out.println(solution0452.findMinArrowShots(points));
 	}
 }
--- JavaR2/src/Solution0452.java
+++ JavaR2/src/Solution0452.java
@@ -1,8 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author maximusyoung
  */
 public class Solution0452 {
 	public int findMinArrowShots(int[][] points) {
+		int length = points.length;
+		if (length == 1) {
+			return 1;
+		}
+		Arrays.sort(points, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] point1, int[] point2) {
+				//return point1[1] - point2[1];
+				return Integer.compare(point1[1], point2[1]);
+			}
+		});
 
+		int i = 0, j = 1;
+		int count = 0;
+		while (i < length && j < length) {
+			if (j == length - 1) {
+				count++;
+			}
+			if (points[j][0] <= points[i][1]) {
+				j++;
+			} else {
+				//开始新一轮的贪心
+				count++;
+				i = j;
+				j = i + 1;
+			}
+		}
+		return count;
 	}
 }
