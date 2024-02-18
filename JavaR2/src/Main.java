6d8c063a29d8d7b46f57e098efe0afdc40c491cc
maximus <1091261998@qq.com>
2024-02-18 11:46:29 +0800

    406

--- JavaR2/src/Main.java
+++ JavaR2/src/Main.java
@@ -27,8 +27,14 @@ public class Main {
 //		int[][] points = new int[][] {{1, 2}, {2,3}, {3,4}, {4,5}};
 //		int[][] points = new int[][] {{2, 3}, {2,3}};
 //		int[][] points = new int[][] {{1, 2}, {3,4}};
-		int[][] points = new int[][] {{-2147483646,-2147483645}, {2147483646,2147483647}};
-		Solution0452 solution0452 = new Solution0452();
-		System.out.println(solution0452.findMinArrowShots(points));
+//		int[][] points = new int[][] {{-2147483646,-2147483645}, {2147483646,2147483647}};
+//		Solution0452 solution0452 = new Solution0452();
+//		System.out.println(solution0452.findMinArrowShots(points));
+		int[][] people = new int[][] {
+			{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}
+		};
+		Solution0406 solution0406 = new Solution0406();
+		solution0406.reconstructQueue(people);
+		System.out.println(solution0406.reconstructQueue(people));
 	}
 }
--- JavaR2/src/Solution0406.java
+++ JavaR2/src/Solution0406.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution0406 {
 	public int[][] reconstructQueue(int[][] people) {
-		
+		Arrays.sort(people, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] p1, int[] p2) {
+				if (p1[0] != p2[0]) {
+					return p2[0] - p1[0];
+				} else {
+					return p1[1] - p2[1];
+				}
+			}
+		});
+		return people;
 	}
 }
