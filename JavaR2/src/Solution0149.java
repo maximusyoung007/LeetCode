ad727a4e063cf995694166004daf1bd6487ed292
maximusyoung <1091261998@qq.com>
2024-08-05 16:17:21 +0800

    149 to do

---
+++ JavaR2/src/Solution0149.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <pre>
+ * Modify Information:
+ * Author       Date          Description
+ * ============ ============= ============================
+ * maximus         2024/8/5      create
+ */
+public class Solution0149 {
+	public int maxPoints(int[][] points) {
+		Map<Double, Integer> slopeMap = new HashMap<>();
+	}
+}
