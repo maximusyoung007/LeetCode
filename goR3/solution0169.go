80ad5d11658d90859b366d14ad8d88c7db1b4eb7
maximusyoung <1091261998@qq.com>
2024-10-22 17:46:10 +0800

    169 todo

---
+++ goR3/solution0169.go
@@ -0,0 +1,15 @@
+package main
+
+// 投票法，候选人初始化为nums[0], count为候选人票数，
+// 相同的+1，不同的-1，因为多数票数量大于n/2, 非多数票小于n/2,
+// 因此全部相减完，多数票还有剩余，剩余的即为结果
+func majorityElement(nums []int) int {
+	res := nums[0]
+	count := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == res {
+			count++
+		}
+	}
+}
