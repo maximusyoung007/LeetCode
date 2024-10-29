054023b192e054090b64a0b3488350d7ba360ac1
maximusyoung <1091261998@qq.com>
2024-10-29 17:50:05 +0800

    135done 42 todo

--- goR3/solution0042.go
+++ goR3/solution0042.go
@@ -2,7 +2,8 @@ package main
 
 //按列求，先取每个位置左边最高的高度，再取每个位置右边最高的高度，取其中的最小值
 //每个位置与最小值的差值即为可以盛水的高度
-func TrapByColumn(height []int) int {
+//动态规划，左边最大的是max{max, height[i]}
+func TrapColumn(height []int) int {
 	res := 0
 	n := len(height)
 	max := -1
@@ -43,4 +44,13 @@ func TrapByColumn(height []int) int {
 	}
 
 	return res
+}
+
+//双指针
+//要找到每个位置左边最大的和右边最大的两个值中更小的那个值
+//lmax,rmax记录每个位置左边和右边最大的值
+//两个指针，一个从左往右遍历，一个从右往左遍历，刚好可以取到左右最大值更小的那个，更新相关值并移动指针
+//如果遍历到的元素大于左右最大值，则更新
+func TrapTwoPoints(height[] int) {
+
 }
--- goR3/solution0135.go
+++ goR3/solution0135.go
@@ -1,30 +1,55 @@
 package main
 
 func Candy(ratings []int) int {
-	//如果是递增序列，则后一个比前一个+1
-	//如果是递减序列，如果前一个-1大于0，则直接加入，否则把递减序列所有的+1
 	n := len(ratings)
 
 	res := 1
+	//递减序列长度
 	num := 1
+	//上一位
 	pre := 1
+	//递减序列第一位
+	first := 0
+
+	//如果是递增序列，则后一个比前一个+1
+	//如果是递减序列，根据first是否大于num,判断是否要把递增序列的每一位+1
+	//如果相等，则后一位默认为1
+	//递增或者相等时，当前位默认为递增序列的第一位，有nums=1，first=pre,如果向后遍历发现不是递减序列，则刷新这两个值
 
 	for i := 1; i < n; i++ {
 		if ratings[i] > ratings[i-1] {
 			pre += 1
 			res += pre
 			num = 1
-		} else if ratings[i] <= ratings[i-1] {
+			first = pre
+		} else if ratings[i] < ratings[i-1] {
 			//递减序列长度+1
-			num++
-			if ratings[i-1] > 1 {
-				pre = pre - 1
-				res += pre
+			if pre > 1 {
+				res += 1
+				pre = 1
+				num++
 			} else {
-				//递减序列的每一个位置都+1
-				res += num
+				num++
+				//记录递减序列的第一个为first，如果first>=num,则first，fisrt-1,first-2..这样的序列一定满足，first不需要增加,否则第一个也要+1
+				//如first=5, 向后遍历依次是
+				//5,1
+				//5,2,1
+				//5,3,2,1
+				//5,4,3,2,1此时first=num,fisrt不需要+1，3,2,1,0->4,3,2,1每一位+1
+				//num=6,大于5，则fisrt也要+1了
+				if first >= num {
+					res += num - 1
+				} else {
+					res += num
+					first++
+				}
 			}
-
+		} else if ratings[i] == ratings[i-1] {
+			//如果后一位等于前一位，后一位从1开始
+			res += 1
+			pre = 1
+			first = pre
+			num = 1
 		}
 	}
 
